1755549950092416bab6294a11be213a005ce538
Escaleirex <69918820+Escaleirex@users.noreply.github.com>
2024-02-29 15:07:46 +0000

    To finish tomorrow

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let searchElement = document.getElementsByClassName("search");
+let result;
+
 searchElement[0].addEventListener("keydown", function (e) {
     if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
         search(e);
@@ -9,7 +11,7 @@ async function search() {
     let text = searchElement.value;
     console.log('Search Value: ' + text);
     if(text = 'Undefined'){
-        console.log('Search Value is undefined, searching for "Confessiosn - USHER" (placeholder) instead!');
+        console.log('Search Value is undefined, searching for "Confessions - USHER" (placeholder) instead!');
         text = "Confessions - USHER";
     }
 
@@ -24,12 +26,32 @@ async function search() {
     
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(JSON.parse(result));
+        result = await response.text();
+        let resultJSON = JSON.parse(result);
+        console.log(resultJSON);
 
-        // $("results-area").append(result.);
+        for(let i = 0; i < 3 ; i++){
+
+            $(".results-area").append("\
+            <div class='result'>\
+                <img src='" + resultJSON.tracks.hits[i].track.share.image + "'>\
+                <div class='song-details-card'>\
+                    <p class='song-title'>" + resultJSON.tracks.hits[i].track.title + "</p>\
+                    <p class='song-artist'>" + resultJSON.tracks.hits[i].track.subtitle + "</p>\
+                </div>\
+            </div>");
+
+            $(".song-details-card").append($("<button type='button' class='btn btn-primary select-button'>Select</button>")).click(details.resultJSON)
+        }
 
     } catch (error) {
         console.error(error);
     }
+}
+
+
+
+function details(resultJSON) {
+    console.log("worked i think: \n", resultJSON);
+    // console.log(resultJSON.tracks.hits[parseInt(i)].track);
 }
