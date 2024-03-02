66bcbfa3ba29ac95a5b45e75a44301e73286cd4c
Escaleirex <69918820+Escaleirex@users.noreply.github.com>
2024-03-02 01:39:58 +0000

    okay i lied i continued to work on the mini-project

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ async function search() {
             btn.type = "button";
             btn.addEventListener("click",function(){
                 console.log("The Song Selected is: " + result.tracks.hits[i].track.title + " - " + result.tracks.hits[i].track.subtitle + "\nShowing Details...");
-                detailsF(result.tracks.hits[i].track.key);
+                detailsF(result.tracks.hits[i].track.key, result.tracks.hits[i].track.hub);
             });
 
             //append everything, i hate js
@@ -83,7 +83,7 @@ async function search() {
     }
 }
 
-async function detailsF(id) {
+async function detailsF(id, providersHub) {
     let container = document.querySelector(".results-area");
     container.innerHTML = ""; 
 
@@ -110,14 +110,21 @@ async function detailsF(id) {
         imageCont.className = "image-container";
         
         let coverArt = document.createElement('img');
-        coverArt.src = "";//insert cover art here
+        coverArt.src = result.resources["shazam-songs"][id].attributes.images.coverArtHq;
         
+        let audio = new Audio();
+        audio.volume = 0.25;
+        audio.loop = false;
+        audio.src = result.resources["shazam-songs"][id].attributes.streaming.preview;
+        audio.pause();
+
         let btnPlay = document.createElement('button');
         btnPlay.className = "button-play";
         btnPlay.type = "button";
         btnPlay.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
         btnPlay.addEventListener("click",function(){
-            console.log("Congrats, the play button was clicked, please change this");
+            console.log("Playing Audio...");
+            playPausePreview(audio);
         });
     
         imageCont.append(coverArt);
@@ -127,17 +134,13 @@ async function detailsF(id) {
         //div with all the shit that needs to be shown to the user
         let songMetadata = document.createElement('div');
         let songTitle = document.createElement('p');
-        songTitle.innerHTML = "test";//insert song title here
+        songTitle.innerHTML = result.resources["shazam-songs"][id].attributes.title;//insert song title here
         songTitle.className = "song-title";
     
         let songArtist = document.createElement('p');
-        songArtist.innerHTML = "test";//insert artist name here aka subtitle
+        songArtist.innerHTML = result.resources.albums[result.resources["shazam-songs"][id].relationships.albums.data[0].id].attributes.artistName + ' • ' + result.resources.albums[result.resources["shazam-songs"][id].relationships.albums.data[0].id].attributes.name + ' • ' + result.resources.albums[result.resources["shazam-songs"][id].relationships.albums.data[0].id].attributes.releaseDate + ' • ' + result.resources["shazam-songs"][id].attributes.genres.primary;
         songArtist.className = "song-artist";
-    
-        let songGenres =  document.createElement('p');
-        songGenres.innerHTML = "test";//insert genres with bullets between them
-        songGenres.className = "genres";
-    
+        
         let listenON = document.createElement('p');
         listenON.innerHTML = "Listen On";
         listenON.className = "listen-on";
@@ -145,57 +148,78 @@ async function detailsF(id) {
         //colocar só para aparecer botão desse serviço se a musica lá estiver disponivel
         let streamingServices = document.createElement('div');
         streamingServices.className = "streaming-services";
+
+        for (let i = 0; i < providersHub.providers.length; ++i) { 
+            if(providersHub.providers[i].type == "SPOTIFY"){
+                let btnSpotify = document.createElement('button');
+                btnSpotify.className = "btn btn-outline-dark streaming-button";
+                btnSpotify.type = "button";
+                btnSpotify.innerHTML = '<i class="bi bi-spotify"></i>';
+                btnSpotify.addEventListener("click", function(){
+                    window.open(providersHub.providers[i].actions[0].uri, "_self");
+                });
+                streamingServices.append(btnSpotify);
+            } else if (providersHub.providers[i].type == "DEEZER"){
+                let btnDeezer = document.createElement('button');
+                btnDeezer.className = "btn btn-outline-dark streaming-button";
+                btnDeezer.type = "button";
+                btnDeezer.innerHTML = '<i class="fa-brands fa-deezer"></i>';
+                btnDeezer.addEventListener("click", function(){
+                    //I CANT GET THIS TO WORK ON WINDOWS/MAC JUST IGNORE THE ERROR!!! 
+                    //WHO USES DEEZER ANYWAYS? JUST USE SPOTIFY! 
+                    //ITS IMPLEMENTED IDK HOW TO GET IT TO WORK BECAUSE DEEZER DOCS ARE BAD AND THIS API GIVES ME A URI AND NOT A URL!!
+                    window.open(providersHub.providers[i].actions[0].uri, "_self");
+                });
+                streamingServices.append(btnDeezer);
+            } 
+        }
+        if (providersHub.options[0].providername == 'applemusic'){
+            let btnApple = document.createElement('button');
+            btnApple.className = "btn btn-outline-dark streaming-button";
+            btnApple.type = "button";
+            btnApple.innerHTML = '<i class="bi bi-apple"></i>';
+            btnApple.addEventListener("click", function(){
+                window.open(providersHub.options[0].actions[1].uri, "_self");
+            });
+            streamingServices.append(btnApple);
+        }
     
-        let btnSpotify = document.createElement('button');
-        btnSpotify.className = "btn btn-outline-dark streaming-button";
-        btnSpotify.type = "button";
-        btnSpotify.innerHTML = '<img class="streaming-logo" src="assets/spotify-logo.svg">';
-        
-        let btnApple = document.createElement('button');
-        btnApple.className = "btn btn-outline-dark streaming-button";
-        btnApple.type = "button";
-        btnApple.innerHTML = '<img class="streaming-logo" src="assets/apple-music-3.svg">';
+        const urlM = 'https://api.musixmatch.com/ws/1.1/track.get?apikey=41bf102baf84ef9e2d9895ee610fa541&commontrack_id=907374';
+        const optionsM = {
+            method: 'GET',
+        };
     
-        let btnDeezer = document.createElement('button');
-        btnDeezer.className = "btn btn-outline-dark streaming-button";
-        btnDeezer.type = "button";
-        btnDeezer.innerHTML = '<img class="streaming-logo" src="assets/deezer-logo.svg">';
-    
-        let btnAmazon = document.createElement('button');
-        btnAmazon.className = "btn btn-outline-dark streaming-button";
-        btnAmazon.type = "button";
-        btnAmazon.innerHTML = '<img class="streaming-logo" src="assets/Amazon_Music_logo.svg">';
-    
-        let btnTidal = document.createElement('button');
-        btnTidal.className = "btn btn-outline-dark streaming-button";
-        btnTidal.type = "button";
-        btnTidal.innerHTML = '<img class="streaming-logo" src="assets/tidal-1.svg">';
-    
-        let btnYoutube = document.createElement('button');
-        btnYoutube.className = "btn btn-outline-dark streaming-button";
-        btnYoutube.type = "button";
-        btnYoutube.innerHTML = '<img class="streaming-logo" src="assets/youtube-6.svg">';
-    
-        streamingServices.append(btnSpotify);
-        streamingServices.append(btnApple);
-        streamingServices.append(btnDeezer);
-        streamingServices.append(btnAmazon);
-        streamingServices.append(btnTidal);
-        streamingServices.append(btnYoutube);
-    
-        let lyricsTitle = document.createElement('p');
-        lyricsTitle.className = "lyrics-title";
-        lyricsTitle.innerHTML = '<a href="' + 'insert link here' + '" target=”_blank” style="text-decoration: none;">\
-                                    Lyrics\
-                                </a>';
+        try {
+            const responseM = await fetch(urlM, optionsM);
+            const resultMJSON = await response.text();
+            const resultM = JSON.parse(resultMJSON);
+            console.log(resultM);
+            let lyricsTitle = document.createElement('p');
+            lyricsTitle.className = "lyrics-title";
+            lyricsTitle.innerHTML = '<a href="' + resultM.message + '" target=”_blank” style="text-decoration: none;">\
+                                        Lyrics\
+                                    </a>';
+        } catch (error) {
+            console.error(error);
+        }
+
+
         
         let lyrics = document.createElement('div');
         lyrics.className = "lyrics";
-        lyrics.innerHTML = 'Insert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\nInsert Lyrics Here\n';
+        let lyricsText = "";
+
+        for (let i = 0; i < result.resources.lyrics[result.resources["shazam-songs"][id].relationships.lyrics.data[0].id]['attributes']['text'].length; ++i) { 
+            const element = result.resources.lyrics[result.resources["shazam-songs"][id].relationships.lyrics.data[0].id]['attributes']['text'][i];
+            lyricsText = lyricsText + element + '<br>';
+        }
+
+        lyricsText = lyricsText + '<br><br>' + result.resources.lyrics[result.resources["shazam-songs"][id].relationships.lyrics.data[0].id]['attributes'].footer;
+
+        lyrics.innerHTML = lyricsText;
     
         songMetadata.append(songTitle);
         songMetadata.append(songArtist);
-        songMetadata.append(songGenres);
         songMetadata.append(listenON);
         songMetadata.append(streamingServices);
         songMetadata.append(lyricsTitle);
@@ -205,5 +229,14 @@ async function detailsF(id) {
 
     } catch (error) {
         console.error(error);
+    }
+}
+
+function playPausePreview(audio){
+    if(audio.duration > 0 && !audio.paused){
+        audio.pause();
+    }
+    else{
+        audio.play();
     }
 }
